71367a28216de81b266b438a7cf0ea9b7ba9f594
GuillaumeGas <guillaume@gas-ntic.fr>
2018-02-28 03:57:05 -0800

    added issue 35

---
+++ issues/issue_35/Teelol/.build/teelol.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/../teelol.dir/main.yr.o"
+  "CMakeFiles/../teelol.dir/src/eventHandler.yr.o"
+  "CMakeFiles/../teelol.dir/src/game.yr.o"
+  "CMakeFiles/../teelol.dir/src/lib/sdl.yr.o"
+  "CMakeFiles/../teelol.dir/src/lib/signal.yr.o"
+  "CMakeFiles/../teelol.dir/src/player.yr.o"
+  "CMakeFiles/../teelol.dir/src/screen.yr.o"
+  "../teelol.pdb"
+  "../teelol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/../teelol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
